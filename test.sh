6cf7fa5d6e092d444357e3cc9916d80f48e12778
gh0o5t <gh0o5t@protonmail.com>
2021-01-29 18:52:27 +0100

    Test

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-check_command(){
-    if [ $? -ne 0 ]; then
-        echo "$1"
-        echo "Installation is not completed"  
-        echo "Press any key to close..."  
-        read
-        exit
-    fi
-}
+#check_command(){
+    #if [ $? -ne 0 ]; then
+        #echo "$1"
+        #echo "Installation is not completed"  
+        #echo "Press any key to close..."  
+        #read
+        #exit
+    #fi
+#}
 
 
-apt-key list 2> /dev/null | grep -q "0EBF CD88" 
-check_command "Docker fingerprint verification failed"
+#apt-key list 2> /dev/null | grep -q "0EBF CD88" 
+#check_command "Docker fingerprint verification failed"
+
+touch a
